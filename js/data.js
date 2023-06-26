d39dde31ef20735c3d8410d42e0f45812503e72d
NataRey <132687313+NataRey@users.noreply.github.com>
2023-06-26 11:00:03 -0500

    cambio imagen 1

--- js/data.js
+++ js/data.js
@@ -5,37 +5,127 @@ const data = [
         id:1,
         nombre: "toma de caucho",
         precio: 6.800,
-        imagen: "../img/10.png"
+        imagen: "./img/1.png"
     },
     {
         id:2,
         nombre: "toma doble tapa aluminio",
         precio: 6.800,
-        imagen: "../img/12.png"
+        imagen: "../img/2.png"
     },
     {
         id:3,
         nombre: "toma + interruptor",
         precio: 6.800,
-        imagen: "../img/2.png"
+        imagen: "../img/3.png"
     },
     {
         id:4,
         nombre: "toma de caucho con polo",
         precio: 6.800,
-        imagen: "../img/14.png"
+        imagen: "../img/4.png"
     },
     {
         id:5,
         nombre: "toma de caucho con polo",
         precio: 6.800,
-        imagen: "../img/14.png"
+        imagen: "../img/5.png"
     },
     {
         id:6,
         nombre: "toma de caucho con polo",
         precio: 6.800,
+        imagen: "../img/6.png"
+    },
+    {
+        id:7,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/7.png"
+    },
+    {
+        id:8,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/8.png"
+    },
+    {
+        id:9,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/9.png"
+    },
+    {
+        id:10,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/10.png"
+    },
+    {
+        id:11,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/11.png"
+    },
+    {
+        id:12,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/12.png"
+    },
+    {
+        id:13,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/13.png"
+    },
+    {
+        id:14,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
         imagen: "../img/14.png"
+    },
+    {
+        id:15,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/15.png"
+    },
+    {
+        id:16,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/16.jpg"
+    },
+    {
+        id:17,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/17.jpg"
+    },
+    {
+        id:18,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/18.jpg"
+    },
+    {
+        id:19,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/19.jpg"
+    },
+    {
+        id:20,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/20.jpg"
+    },
+    {
+        id:21,
+        nombre: "toma de caucho con polo",
+        precio: 6.800,
+        imagen: "../img/21.jpg"
     },
 
 ]
